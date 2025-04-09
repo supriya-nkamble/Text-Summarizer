7f5ca3b80cc964e8dd567d6dffcd14fa6e238f44
supriya-nkamble <52688258+supriya-nkamble@users.noreply.github.com>
2025-04-09 21:13:40 +1000

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from text_summarizer.pipeline.stage_01_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
+from text_summarizer.pipeline.stage_02_data_validation import (
+    DataValidationTrainingPipeline,
+)
 from text_summarizer.logging import logger
 
 
@@ -10,6 +13,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion_training_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_training_pipeline.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation_training_pipeline = DataValidationTrainingPipeline()
+    data_validation_training_pipeline.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
