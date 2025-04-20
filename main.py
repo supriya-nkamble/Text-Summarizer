395c7acc6ff21e1f2b44d0489cb315c0ed06b8a2
supriya-nkamble <52688258+supriya-nkamble@users.noreply.github.com>
2025-04-20 12:00:01 +1000

    adding transformation

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from text_summarizer.pipeline.stage_01_data_ingestion import (
 from text_summarizer.pipeline.stage_02_data_validation import (
     DataValidationTrainingPipeline,
 )
+from text_summarizer.pipeline.stage_03_data_transformation import (
+    DataTransformationTrainingPipeline,
+)
 from text_summarizer.logging import logger
 
 
@@ -25,6 +28,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation_training_pipeline = DataValidationTrainingPipeline()
     data_validation_training_pipeline.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_transformation_training_pipeline = DataTransformationTrainingPipeline()
+    data_transformation_training_pipeline.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
